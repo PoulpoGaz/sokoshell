00f1c53f14347ddc2040e548431989dee244ba4e
darth-mole <trash-spamaps@mail.com>
2022-12-11 21:56:12 +0100

    Started implementing Greedy Heuristic (added source files)

---
+++ sokoshell/src/test/java/fr/valax/sokoshell/solver/heuristic/GreedyHeuristicTest.java
@@ -0,0 +1,35 @@
+package fr.valax.sokoshell.solver.heuristic;
+
+import fr.poulpogaz.json.JsonException;
+import fr.valax.sokoshell.graphics.MapRenderer;
+import fr.valax.sokoshell.graphics.MapStyleReader;
+import fr.valax.sokoshell.readers.PackReaders;
+import fr.valax.sokoshell.solver.Level;
+import fr.valax.sokoshell.solver.Map;
+import fr.valax.sokoshell.solver.Pack;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.util.Arrays;
+
+public class GreedyHeuristicTest {
+
+    @Test
+    void distancesTest() throws java.io.IOException, JsonException {
+
+        Pack pack = PackReaders.read(Path.of("../levels/TIPEex.8xv"), false);
+
+        Level level = pack.getLevel(0);
+        Map map = level.getMap();
+
+        MapRenderer mR = new MapRenderer();
+        mR.setStyle(new MapStyleReader().read(Path.of("../styles/isekai/isekai.style")));
+
+        map.computeFloors();
+        map.computeDeadTiles();
+        //mR.sysPrint(map, level.getPlayerX(), level.getPlayerY());
+
+        GreedyHeuristic h = new GreedyHeuristic(map);
+        System.out.println(h);
+    }
+}
